060c0fded7fbae8b2bd2e28a3b0819aa290775ca
David Little <david.frank.little@gmail.com>
2018-12-04 13:48:22 -0500

    Improved plotting for cortical representation

--- src/plot_axes.jl
+++ src/plot_axes.jl
@@ -3,5 +3,12 @@ function PlotAxes.asplotable(x::AuditorySpectrogram,args...;kwds...)
   x = AxisArray(Array(x),AxisArrays.axes(x,1),logfreqs)
   asplotable(x,args...;kwds...)
 end
+
+function PlotAxes.asplotable(x::Cortical,args...;kwds...)
+  logfreqs = Axis{:logfreq}(log.(ustrip.(freqs(x))))
+  x = AxisArray(Array(x),AxisArrays.axes(x)[1:end-1]...,logfreqs)
+  asplotable(x,args...;kwds...)
+end
+
             
             
